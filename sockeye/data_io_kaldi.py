572382715651e88907c95efd5bd669bdf510de97
chenzhehuai@foxmail.com <chenzhehuai@foxmail.com>
2018-01-10 07:35:24 +0800

    cac acc in training; move lab +3/4; pipeline to scoring; vocab

--- sockeye/data_io_kaldi.py
+++ sockeye/data_io_kaldi.py
@@ -930,7 +930,7 @@ def read_content(path: str, itype: str, limit: Optional[int] = None) -> Iterator
             yield mat
     elif itype == "lab":
         for key,mat in kaldi_io.read_vec_int_ark(path):
-            yield mat+1 #make it start from 1: in decoder, use # of !=0 to count length
+            yield mat+4 #used to make it start from 1: in decoder, use # of !=0 to count length; <pad> <unk> <s> </s>, so add 4
     else:
         with smart_open(path) as indata:
             for i, line in enumerate(indata):
